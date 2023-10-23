1d22c5f7c222231cea11a038ab530175d9763b11
MartinInformatikk <martinmalo2003@gmail.com>
2023-10-23 16:32:49 +0200

    Lagde test for skriving til JSON, og la inn fiks for async error som gjorde at test ikke funket

--- backend/create_tour.js
+++ backend/create_tour.js
@@ -1,51 +1,52 @@
 const fs = require("fs");
 
 function writeToJSON(tours) {
+  return new Promise((resolve, reject) => {
+    const toursToWrite = JSON.stringify(tours, null, 2);
 
-  const toursToWrite = JSON.stringify(tours, null, 2);
-    
-  fs.writeFile('backend/JSON/tours.json', toursToWrite, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing the file:', err);
-    } else {
-      console.log('Data has been written to tours.json');
-      
-    }
-  });
-  }
-
-function readFromJSON(){
-
-  fs.readFile('backend/JSON/tours.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading the file:', err);
-      return;
-    }
-  
-    const jsonData = JSON.parse(data);
-    console.log(jsonData);
+    fs.writeFile('backend/JSON/tours.json', toursToWrite, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing the file:', err);
+        reject(err);
+      } else {
+        console.log('Data has been written to tours.json');
+        resolve();
+      }
+    });
   });
 }
 
-  class Tour {
-    constructor(name, price) {
-      this.name = name;
-      this.price = price;
-    }
-  }
-
-  
-  var testTur1 = new Tour('tur 1', 2000);
-  var testTur2= new Tour('tur 2', 4500);
-
-  const tours = [testTur1, testTur2];
-
-  writeToJSON(tours);
-
-
-  readFromJSON();
-
-  if (typeof window === "undefined"){ // Vis vi skjører i node.js
-    // Eksportering til test
-    module.exports = {writeToJSON: writeToJSON,readFromJSON: readFromJSON};
+function readFromJSON() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('backend/JSON/tours.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading the file:', err);
+        reject(err);
+      } else {
+        const jsonData = JSON.parse(data);
+        console.log(jsonData);
+        resolve(jsonData);
+      }
+    });
+  });
 }
+
+class Tour {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+var testTur1 = new Tour('tur 1', 2000);
+var testTur2 = new Tour('tur 2', 4500);
+
+const tours = [testTur1, testTur2];
+
+if (typeof window === "undefined") {
+  // Vis vi skjører i node.js
+  // Eksportering til test
+  module.exports = { writeToJSON: writeToJSON, readFromJSON: readFromJSON };
+}
+
+module.exports = { writeToJSON, readFromJSON };
--- unit-testing/create_tour.test.js
+++ unit-testing/create_tour.test.js
@@ -1,5 +1,21 @@
 const create_tourModule = require("../backend/create_tour");
 
-test("Tester skriving til JSON", () => {
-    expect(create_tourModule.writeToJSON(testTur)).toBe("Data has been written to tours.json");
-});
+test("Test writing to JSON", async () => {
+
+    class Tour {
+        constructor(name, price) {
+          this.name = name;
+          this.price = price;
+        }
+      }
+      
+    const testTour1 = new Tour('tur 1', 2000);
+    const testTour2 = new Tour('tur 2', 4500);
+    const tours = [testTour1, testTour2];
+  
+    await create_tourModule.writeToJSON(tours);
+  
+    const jsonData = await create_tourModule.readFromJSON();
+  
+    expect(jsonData).toEqual(tours);
+  });
