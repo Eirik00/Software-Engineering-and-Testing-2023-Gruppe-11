00641ecabef18fcbe3325df7ec1e1bb78b660901
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2023-11-08 16:56:24 +0100

    Prøver å sette opp localstorage mock

---
+++ unit-testing/setupTests.js
@@ -0,0 +1,27 @@
+global.localStorage = (function () {
+    let store = {};
+
+    return {
+        getItem(key) {
+            return store[key];
+        },
+
+        setItem(key, value) {
+            store[key] = value;
+        },
+
+        clear() {
+            store = {};
+        },
+
+        removeItem(key) {
+            delete store[key];
+        },
+
+        getAll() {
+            return store;
+        },
+    };
+})();
+
+Object.defineProperty(window, "mock", { value: localStorage });
