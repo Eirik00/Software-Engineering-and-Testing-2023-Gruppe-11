c79666dc93d15c5aafc1d972fc86b811ca26e904
MartinInformatikk <martinmalo2003@gmail.com>
2023-11-08 16:50:04 +0100

    Legger ny opprettede tours til å vises på hovedsiden, og flytter script til get_tours.js

--- backend/get_tours.js
+++ backend/get_tours.js
@@ -1,3 +1,23 @@
 //predefined tours
 
+var allTours = [];
+            allTours = JSON.parse(localStorage.getItem("tourList"));
+            allTours.reverse(); //slik at de nyligst opprettede turene vises, fremfor de eldste
 
+            const displayNames = ["name1", "name2", "name3", "name4"];
+            const displayDesc = ["desc1", "desc2", "desc3", "desc4"];
+            const displayPrices = ["price1", "price2", "price3", "price4"];
+
+            for (let i = 0; i < localStorage.getItem("tourList").length; i++){
+                let tempName = document.getElementById(displayNames[i]);
+                tempName.innerHTML = allTours[i].name;
+
+                let tempDesc = document.getElementById(displayDesc[i]);
+                tempDesc.innerHTML = allTours[i].description;
+
+                let tempPrice = document.getElementById(displayPrices[i]);
+                tempPrice.innerHTML = "pris: " + allTours[i].price;
+            }
+
+
+            console.log("funker");
