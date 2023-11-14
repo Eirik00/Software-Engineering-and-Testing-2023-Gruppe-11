00e7dc01e8c4ffb9f6df543a497fdcb3f14f919b
MartinInformatikk <martinmalo2003@gmail.com>
2023-11-14 17:07:07 +0100

    Viser nå alle tours i tillegg, mangler css

--- backend/get_tours.js
+++ backend/get_tours.js
@@ -1,15 +1,13 @@
 //predefined tours
-
-var allTours = [];
-            allTours = JSON.parse(localStorage.getItem("tourList"));
+            const allTours = JSON.parse(localStorage.getItem("tourList"));
             allTours.reverse(); //slik at de nyligst opprettede turene vises, fremfor de eldste
 
             const displayNames = ["name1", "name2", "name3", "name4"];
             const displayDesc = ["desc1", "desc2", "desc3", "desc4"];
             const displayPrices = ["price1", "price2", "price3", "price4"];
-            const displayAccessibility = ["accessibility1", "accessibility2", "accessibility3", "accessibility4"]
+            const displayAccessibility = ["accessibility1", "accessibility2", "accessibility3", "accessibility4"];
 
-            for (let i = 0; i < localStorage.getItem("tourList").length; i++){
+            for (let i = 0; i < displayNames.length; i++){
                 let tempName = document.getElementById(displayNames[i]);
                 tempName.innerHTML = allTours[i].name;
 
@@ -17,7 +15,7 @@ var allTours = [];
                 tempDesc.innerHTML = allTours[i].description;
 
                 let tempPrice = document.getElementById(displayPrices[i]);
-                tempPrice.innerHTML = "pris: " + allTours[i].price;
+                tempPrice.innerHTML = "pris: " + allTours[i].price + ".- kr";
 
                 let tempAccessibility = document.getElementById(displayAccessibility[i]);
               
@@ -26,6 +24,44 @@ var allTours = [];
                 }
                 else {tempAccessibility.innerHTML = "Handicap-Tilrettelagt: Nei"}
             }
+
+            function displayAllTours(tour){
+                let newdiv = document.createElement("div");
+                newdiv.classList.add("tour");
+
+                let newdivName = document.createElement("h2");
+                newdivName.classList.add("name");
+                newdivName.innerHTML = tour.name;
+                newdiv.appendChild(newdivName);
+
+                let newdivDesc = document.createElement("p");
+                newdivDesc.classList.add("desc");
+                newdivDesc.innerHTML = tour.description;
+                newdiv.appendChild(newdivDesc);
+
+                let newdivPrice = document.createElement("p");
+                newdivPrice.classList.add("price");
+                newdivPrice.innerHTML = "Pris: " + tour.price + ".- kr";
+                newdiv.appendChild(newdivPrice);
+
+                let newdivAcc = document.createElement("p");
+                newdivAcc.classList.add("accessibility");
+                if (tour.accessibility == true){
+                    newdivAcc.innerHTML = "Handicap-Tilrettelagt: Ja"
+                }
+                else {newdivAcc.innerHTML = "Handicap-Tilrettelagt: Nei"}
+                newdiv.appendChild(newdivAcc);
+
+                
+                let allToursdiv = document.getElementById("Alltours");
+                allToursdiv.appendChild(newdiv);
+            }
+
+            
+            for (let i = 0; i < allTours.length; i++) {
+                displayAllTours(allTours[i]);
+            }
+            
 
 
             console.log("funker");
