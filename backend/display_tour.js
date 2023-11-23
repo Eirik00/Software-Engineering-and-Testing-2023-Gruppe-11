a58fb05a4f85993be71dd97ba7d71c317aede5a1
Eirik00 <eirik.rene@gmail.com>
2023-11-23 13:15:54 +0100

    små ting

--- backend/display_tour.js
+++ backend/display_tour.js
@@ -3,6 +3,7 @@ const tourList = JSON.parse(localStorage.getItem("tourList"));
 const tourName = document.getElementById("tourName");
 const tourDesc = document.getElementById("tourDesc");
 const tourPrice = document.getElementById("tourPrice");
+const tourOwner = document.getElementById("tourOwner");
 const tourImg = document.getElementById("tourImg");
 let dispTour = "";
 
@@ -16,6 +17,7 @@ for(let i=0;i<tourList.length;i++){
 tourName.innerHTML = dispTour.name;
 tourDesc.innerHTML = dispTour.description;
 tourPrice.innerHTML = dispTour.price + ".- NOK";
+tourOwner.innerHTML = "By: " + dispTour.owner_id;
 try{
     if (process.env.NODE_ENV === "test") {
         module.exports = {
