92cf3c3b9dae546dfb1ccc64ace11653ae88f7c7
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2023-11-23 11:44:02 +0100

    mer testing av getfromform

--- unit-testing/getfromform.test.js
+++ unit-testing/getfromform.test.js
@@ -1,13 +1,14 @@
 const formMod = require ("../backend/getfromform");
 const fs = require("fs");
+const ct = require("../backend/createtour.js")
+
 const setLocalStorage = (id, data) => {
     window.mock.setItem(id, JSON.stringify(data));
 };
 
 describe("testing errors", () => {
 
-    test("tur opprettet", () => {
-        let testtourlist = {};
+    test("Tour created", () => {
         document.body.innerHTML = fs.readFileSync("./Nettside/new-tour/index.html", 'utf-8');
         document.getElementById('name').value = 'Testtour';
         document.getElementById('description').value = 'Koselig testtur med TestTore';
@@ -23,5 +24,19 @@ describe("testing errors", () => {
         expect(error.innerHTML).toBe('* Tur opprettet');
     });
 
-    
-})
+    test("All fields must be filled", () => {
+        document.body.innerHTML = fs.readFileSync("./Nettside/new-tour/index.html", 'utf-8');
+        document.getElementById('name').value = 'Testtour';
+        document.getElementById('description').value = '';
+        document.getElementById('price').value = '';
+        document.getElementById('accessibility').checked = true;
+        const fileInput = document.getElementById('img');
+        const file = new File([''], 'test.txt', { type: 'text/plain' });
+        Object.defineProperty(fileInput, 'files', {
+            value: [file],
+        });
+        formMod.saveTour(false);
+        let error = document.getElementById('errormsg')
+        expect(error.innerHTML).toBe('* Du må fylle ut alle feltene!');
+    });
+});
